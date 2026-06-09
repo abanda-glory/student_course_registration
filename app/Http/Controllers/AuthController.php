1236d6bc801316bf1b89cb14f52502bd6628070a
Abanda Glory <abandaglory91@gmail.com>
2026-06-09 15:21:47 +0100

    Finished creating authentication endpoints

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -28,5 +29,53 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user
         ], 201);
+    }
+
+    // Login
+    public function login(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // verify user by email
+        $user =  User::where('email', $request->email)->first();
+
+        // User not found
+        if (!$user) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        // Wrong password
+        $password = Hash::check($request->password, $user->password);
+
+        if (!$password) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        // Delete old tokens - a user logging in again should get a fresh token
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token'  => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ], 200);
+    }
+
+    // Logout
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logout Successful'
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,14 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// Registration route
 Route::post('/register', [AuthController::class, 'register']);
+
+// Login route
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes - only authenticated users can access
+Route::middleware('auth:sanctum')->group(function () {
+    // Logout route
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
