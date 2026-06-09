81248e78868462189db2932085498503073c7f96
Abanda Glory <abandaglory91@gmail.com>
2026-06-09 14:12:51 +0100

    Created my first endpoint /register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // Registration
+    public function register(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => 'required|confirmed|min:8'
+        ]);
+
+        // Create the user in the database
+        $user = User::create($validated);
+
+        // Logs user automatically and generate token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ], 201);
+    }
 }
